9a49e281d35fc9ca73650765692ba64ec7ac03c5
NLYanick <yanick.plaggenmarsch@gmail.com>
2026-07-04 18:07:10 +0200

    See user profile + add/remove friend button

--- frontend/src/react/components/room/MemberItem.jsx
+++ frontend/src/react/components/room/MemberItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import UserStatus from "../../../enums/user-status";
 import ProfileIcon from "../profile/ProfileIcon"
 
@@ -6,10 +7,20 @@ function MemberItem({ member, isOwner }) {
 
   return (
     <>
-      <li className={`rounded-lg shadow-lg py-2 border-2 ${isOwner ? "border-yellow-500" : "border-gray-300"} flex items-center justify-between px-4`}>
-        <ProfileIcon imgSrc={member.avatar_url} />
-        <p>{member.username}</p>
-        <div className={`min-w-3 min-h-3 rounded-full ${statusClass}`} />
+      <li>
+        {member.disabled ? (
+          <div className={`rounded-lg shadow-lg py-2 border-2 ${isOwner ? "border-yellow-500" : "border-gray-300"} flex items-center justify-between px-4`}>
+            <ProfileIcon imgSrc={member.avatar_url} />
+            <p>{member.username}</p>
+            <div className={`min-w-3 min-h-3 rounded-full ${statusClass}`} />
+          </div>
+        ) : (
+          <Link to={`/profile/${member.uid}`} className={`rounded-lg shadow-lg py-2 border-2 ${isOwner ? "border-yellow-500" : "border-gray-300"} flex items-center justify-between px-4 hover:bg-(--primary-color-light) rounded-l-lg`}>
+            <ProfileIcon imgSrc={member.avatar_url} />
+            <p>{member.username}</p>
+            <div className={`min-w-3 min-h-3 rounded-full ${statusClass}`} />
+          </Link>
+        )}
       </li>
     </>
   )
--- frontend/src/react/pages/UserProfile.jsx
+++ frontend/src/react/pages/UserProfile.jsx
@@ -5,11 +5,17 @@ import { useAuth } from "../AuthUserContext";
 import Button from "../components/Button";
 import ProfileIcon from "../components/profile/ProfileIcon";
 import ProfileDetails from '../components/profile/ProfileDetails';
+import Modal from '../components/Modal';
+import { emitEvent } from '../utils/socket-client';
 
 function UserProfile() {
   const [error, setError] = useState(null);
+  const [friendRequestError, setFriendRequestError] = useState(null);
 
   const [userShowing, setUserShowing] = useState(null);
+
+  const [showModal, setShowModal] = useState(false);
+  const [requestSend, setRequestSend] = useState(false);
 
   const { user } = useAuth();
   const navigate = useNavigate();
@@ -19,12 +25,14 @@ function UserProfile() {
     navigate("/profile/me");
   }
 
+  const isFriend = user && userShowing && userShowing.friends.includes(user.uid);
+
   useEffect(() => {
     async function fetchUser() {
       try {
         const { json } = await sendRequest(`/users/${userId}`, 'GET');
 
-        if(!json.success) {
+        if (!json.success) {
           setError(json.error);
           return;
         } else if (json.user.disabled) {
@@ -41,7 +49,46 @@ function UserProfile() {
     fetchUser();
   }, []);
 
-  if (error) throw new Error(error.message);
+  if (error) throw new Error(error);
+
+  const addFriend = async (username) => {
+    console.log("Adding friend:", username);
+    try {
+      const { json, status } = await sendRequest(`/friend-requests`, "POST", { sender_id: user.uid, recipient_name: username });
+
+      if (!json.success) {
+        setFriendRequestError(json.error || "Failed to add friend. Please try again.");
+        return;
+      }
+
+      emitEvent('notification', { user_id: json.friend_request.recipient, notification: `You have a new friend request from ${user.username}` });
+
+      setShowModal(false);
+      setRequestSend(true);
+      setFriendRequestError(null);
+    } catch (error) {
+      console.error("Error adding friend:", error);
+      setError("Failed to add friend. Please try again.");
+    }
+  }
+
+  const removeFriend = async (friendId) => {
+    try {
+      const { json } = await sendRequest(`/users/${user.uid}/friends/${friendId}`, "DELETE");
+
+      if (!json.success) {
+        setFriendRequestError("Failed to remove friend: " + json.error);
+        return;
+      }
+
+      setShowModal(false);
+
+      window.location.reload();
+    } catch (error) {
+      console.error("Error removing friend:", error);
+      setError("Failed to remove friend. Please try again.");
+    }
+  };
 
   return (
     <>
@@ -50,13 +97,52 @@ function UserProfile() {
 
         <div className='bg-(--primary-color-light) rounded-lg shadow-lg p-6 text-left flex flex-col gap-8'>
           <h2 className='text-2xl text-center font-semibold'>{userShowing?.username}</h2>
-          
+
           <div className='flex justify-center'>
             <ProfileIcon imgSrc={userShowing?.avatar_url} size={userShowing?.avatar_url ? 'large' : 'medium'} />
           </div>
         </div>
 
         <ProfileDetails user={userShowing} />
+
+        <div className='bg-(--primary-color-light) rounded-lg shadow-lg p-6 text-left flex flex-col gap-8'>
+          <h2 className='text-2xl text-center font-semibold'>{isFriend ? 'Remove' : 'Add'} Friend</h2>
+
+          {isFriend ? (
+            <>
+              <div className='flex justify-center'>
+                <Button label="Remove Friend" type="error" onClick={() => setShowModal(true)} />
+              </div>
+
+              {showModal && (
+                <Modal
+                  onClose={() => setShowModal(false)}
+                  footer={
+                    <div className="flex gap-8">
+                      <Button label="Remove" type="error" onClick={() => removeFriend(userShowing?.uid)} />
+                      <Button label="Cancel" type="secondary" onClick={() => setShowModal(false)} />
+                    </div>
+                  }
+                >
+                  <h3 className="text-xl font-bold mb-2">Confirm Removal</h3>
+                  <p className="text-center">Are you sure you want to remove this user from your friends list?</p>
+                </Modal>
+              )}
+            </>
+          ) : (
+            requestSend ? (
+              <p className='text-center text-green-500'>Friend request sent!</p>
+            ) : (
+              <>
+                <div className='flex justify-center'>
+                  <Button label="Add Friend" type="primary" onClick={() => addFriend(userShowing?.username)} />
+                </div>
+
+                {friendRequestError && <p className='text-center text-red-500'>{friendRequestError}</p>}
+              </>
+            )
+          )}
+        </div>
       </div>
     </>
   );
