45cff1a306878aabd3265912d55aa4df8ee54fa4
NLYanick <yanick.plaggenmarsch@gmail.com>
2026-07-14 14:49:09 +0200

    Update TODO + Removed comment

--- frontend/src/react/components/profile/ProfileEditor.jsx
+++ frontend/src/react/components/profile/ProfileEditor.jsx
@@ -29,7 +29,6 @@ const Editor = forwardRef(({ imageSrc, scale, rotate }, ref) => {
 
 
 function ProfileEditor({ imageSrc, ref }) {
-
   const [scale, setScale] = useState(1);
   const [rotate, setRotate] = useState(0);
 
@@ -41,14 +40,6 @@ function ProfileEditor({ imageSrc, ref }) {
         scale={scale}
         rotate={rotate}
       />
-      {/* <AvatarEditor
-        ref={editorRef}
-        image={imageSrc}
-        borderRadius={100}
-        border={5}
-        scale={scale}
-        rotate={rotate}
-      /> */}
 
       <div className='flex flex-col gap-1'>
         <input type="range" name="state" min="0.5" step={0.25} max="5" value={scale} onChange={(e) => setScale(e.target.value)} className='w-48' />
